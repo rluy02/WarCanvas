caac00e4b92d16a48db7cd88e1a4694cd3caafce
kokote888 <diegojimenezg80@gmail.com>
2025-11-05 17:28:30 +0100

    Comandantes hechos, se mueven como el rey en el ajedrez

---
+++ docs/src/Comandante.js
@@ -0,0 +1,7 @@
+import Pieza from './Pieza.js';
+
+export default class Comandante extends Pieza {
+    constructor(fil, col, jugador) {
+        super('Comandante', fil, col, jugador, 4);
+    }
+}
